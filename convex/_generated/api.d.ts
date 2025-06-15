68cdc2963131eeb021322f4dc56b9d7431aa073c
Theo Browne <me@t3.gg>
2025-06-15 13:05:32 -0700

    starting auth

--- convex/_generated/api.d.ts
+++ convex/_generated/api.d.ts
@@ -13,7 +13,9 @@ import type {
   FilterApi,
   FunctionReference,
 } from "convex/server";
+import type * as auth from "../auth.js";
 import type * as data from "../data.js";
+import type * as http from "../http.js";
 
 /**
  * A utility for referencing Convex functions in your app's API.
@@ -24,7 +26,9 @@ import type * as data from "../data.js";
  * ```
  */
 declare const fullApi: ApiFromModules<{
+  auth: typeof auth;
   data: typeof data;
+  http: typeof http;
 }>;
 export declare const api: FilterApi<
   typeof fullApi,
