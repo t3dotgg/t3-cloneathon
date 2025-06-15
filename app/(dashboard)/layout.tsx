2e96e18bd1d7c71007019c89fc2751f7512f81c8
Theo Browne <me@t3.gg>
2025-06-15 14:44:53 -0700

    Working auth with clerk

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,10 +1,30 @@
 import type React from "react";
 import { ConvexClientProvider } from "./convex-provider";
 
+import {
+  ClerkProvider,
+  SignInButton,
+  SignUpButton,
+  SignedIn,
+  SignedOut,
+  UserButton,
+} from "@clerk/nextjs";
+
 export default function DashboardLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  return <ConvexClientProvider>{children}</ConvexClientProvider>;
+  return (
+    <ClerkProvider>
+      <SignedOut>
+        <SignInButton />
+        <SignUpButton />
+      </SignedOut>
+      <SignedIn>
+        <UserButton />
+        <ConvexClientProvider>{children}</ConvexClientProvider>
+      </SignedIn>
+    </ClerkProvider>
+  );
 }
--- convex/data.ts
+++ convex/data.ts
@@ -3,6 +3,12 @@ import { query } from "./_generated/server";
 export const getSubmissions = query({
   args: {},
   handler: async (ctx) => {
+    const identity = await ctx.auth.getUserIdentity();
+    if (identity === null) {
+      throw new Error("Not authenticated");
+    }
+
+    console.log(identity);
     const submissions = await ctx.db.query("submissions").take(100);
     return submissions;
   },
