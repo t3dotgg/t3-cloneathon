6ec008ae6bbf94b15be4498371c7aa43513187c8
Theo Browne <me@t3.gg>
2025-06-15 17:28:27 -0700

    better auth states

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,5 +1,13 @@
+"use client";
+
 import type React from "react";
 import { ConvexClientProvider } from "./convex-provider";
+import {
+  Authenticated,
+  Unauthenticated,
+  AuthLoading,
+  useQuery,
+} from "convex/react";
 
 import {
   ClerkProvider,
@@ -18,19 +26,28 @@ export default function DashboardLayout({
 }) {
   return (
     <ClerkProvider>
-      <SignedOut>
-        <div className="flex justify-center items-center min-h-screen">
-          <SignInButton>
-            <Button className="bg-white text-black">Sign In With GitHub</Button>
-          </SignInButton>
-        </div>
-      </SignedOut>
-      <SignedIn>
-        <div className="flex flex-col min-h-screen bg-black">
-          <TopNav />
-          <ConvexClientProvider>{children}</ConvexClientProvider>
-        </div>
-      </SignedIn>
+      <ConvexClientProvider>
+        <AuthLoading>
+          <div className="flex justify-center items-center min-h-screen">
+            <div className="animate-spin rounded-full h-32 w-32 border-t-2 border-b-2 border-purple-500"></div>
+          </div>
+        </AuthLoading>
+        <Unauthenticated>
+          <div className="flex justify-center items-center min-h-screen">
+            <SignInButton>
+              <Button className="bg-white text-black">
+                Sign In With GitHub
+              </Button>
+            </SignInButton>
+          </div>
+        </Unauthenticated>
+        <Authenticated>
+          <div className="flex flex-col min-h-screen bg-black">
+            <TopNav />
+            {children}
+          </div>
+        </Authenticated>
+      </ConvexClientProvider>
     </ClerkProvider>
   );
 }
